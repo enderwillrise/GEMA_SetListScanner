ddec3871dc7b64d20aa4903d82a1f1fe7f48949c
“[jiahui <[leejiahui12@gmail.com]>
2021-05-18 18:10:00 +0200

    delete previous backend

--- iOS App/backend/loginpage.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from app.models import User, Setlist, Event, ProgrammeManager, Organiser, Interpreter, Song
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Setlist': Setlist, 'Event': Event, 'ProgrammeManager': ProgrammeManager, 'Organiser': Organiser, 'Interpreter': Interpreter, 'Song': Song}
--- iOS App/backend/readme.md
+++
@@ -1,8 +0,0 @@
-# To-do list
-
-1. Open cheatsheet.md for quick code references
-2. Install Flask and Packages
-3. Set up virtual environment
-4. Create database
-5. Create mock data
-6. Run codes
