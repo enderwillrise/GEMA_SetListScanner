e9d19adddf8147c4b311089de7678d286e0deb5b
jiahui <leejiahui12@gmail.com>
2021-06-06 11:06:54 +0200

    additional codes for ocr

--- backend/OCRprogram/Uploading/ocr.py
+++ backend/OCRprogram/Uploading/ocr.py
@@ -12,3 +12,20 @@ def ocr_core(filename):
     return text
 
 print(ocr_core('/Users/abdullahtayyab/Desktop/GEMA_SetListScanner/iOSApp/fronted/SetListScannerFrontend/test2.jpeg'))
+
+
+''' 
+# additional codes for data cleaning / processing
+
+dir = '/Users/leejiahui/GEMA_SetListScanner/backend/uploads' # specify directory to retrieve file
+
+for filename in os.listdir(dir):
+    fullname = os.path.join(dir, filename)  # configure filepath
+    c = ocr_core(fullname)                  # OCR processing
+    d = c.splitlines() # split string into arrays
+    e = [a for a in d if a.strip()]   # remove empty spaces
+    
+    for i in e:                       # each item in array
+        f = OCROutput(content = i)   # store into database
+        db.session.add(f)
+        db.session.commit()     '''
